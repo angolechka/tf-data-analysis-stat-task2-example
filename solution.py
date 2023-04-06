757e489a896bf29a3b0dbbba5f072576159da667
Anna Golubeva <110536476+angolechka@users.noreply.github.com>
2023-04-06 23:34:05 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -9,16 +9,11 @@ from statistics import variance
 chat_id = 266642884 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
     alpha = 1 - p
-    s2 = variance(x)
-    s = sqrt(s2)
-    q = sqrt((len(x) - 1) / chi2.ppf(alpha / 2, len(x) - 1)) - 1
-
-    left = s * (1 - q) / sqrt(3)
-    right = s * (1 + q) / sqrt(3)
-
-    return left, right
+    size = len(x)
+    left = np.sqrt(size * (x ** 2).mean() / (3 * chi2.ppf(q=1 - alpha / 2, df=2 * size)))
+    right = np.sqrt(size * (x ** 2).mean() / (3 * chi2.ppf(q=alpha / 2, df=2 * size)))
+    
+    return left, \
+           right
 
