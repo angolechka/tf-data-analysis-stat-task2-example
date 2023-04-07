9a00bf03ee5e1f3636609935b3f81bea861105cf
Anna Golubeva <110536476+angolechka@users.noreply.github.com>
2023-04-07 10:02:07 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,14 @@
 import pandas as pd
 import numpy as np
-from math import sqrt 
-
-from scipy.stats import chi2, variation, norm
-from statistics import variance
+from scipy.stats import chi2
 
 
 chat_id = 266642884 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
+    # Измените код этой функции
+    # Это будет вашим решением
+    # Не меняйте название функции и её аргументы
     alpha = 1 - p
     size = len(x)
     left = np.sqrt(size * (x ** 2).mean() / (3 * chi2.ppf(q=1 - alpha / 2, df=2 * size)))
@@ -16,4 +16,3 @@ def solution(p: float, x: np.array) -> tuple:
     
     return left, \
            right
-
